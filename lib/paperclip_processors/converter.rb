b497e7b84990825c27ffba050e52ebc305cc253b
Jackson Lynch <jacksonthatsme@gmail.com>
2014-07-23 09:09:03 -0400

    upload and display video sucessful

--- lib/paperclip_processors/converter.rb
+++ lib/paperclip_processors/converter.rb
@@ -3,10 +3,14 @@ module Paperclip
 
 		attr_accessor :video, :photo
 
-		def initialize
-			def convert_video_command
-				Voyeur::Media.new( filename: path_to_file ).convert_to_html5
-			end
+		def initialize(file, options = {},attachment = nil)
+		  super
+		  @file = file
+		end
+
+		def make
+			Voyeur::Media.new( filename: @file ).convert_to_html5
+			@file
 		end
 
 	end
